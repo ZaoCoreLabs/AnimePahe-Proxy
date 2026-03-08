e6e1dbf6bd634cab3e0fc718cc3078437c94af5a
scribl <walzspizzas@gmail.com>
2026-03-08 20:30:58 +0000

    fix .env issues

--- config.js
+++ config.js
@@ -1,3 +1,7 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
 export const CONFIG = {
     PORT: process.env.PORT || 3000,
     ALLOWED_ORIGINS: process.env.ALLOWED_ORIGINS ? process.env.ALLOWED_ORIGINS.split(',') : [],
